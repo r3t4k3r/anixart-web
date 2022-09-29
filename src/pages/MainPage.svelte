1501489ce065c733d45192dfcf6a15aae2a8c038
r3t4k3r <av.19@list.ru>
2022-09-30 01:04:08 +0300

    Добавление функционала "Коллекции недели" и "Смотрят сейчас"

--- src/pages/MainPage.svelte
+++ src/pages/MainPage.svelte
@@ -3,12 +3,17 @@
     import { onMount } from "svelte";
     import { Swiper, SwiperSlide } from "swiper/svelte";
     import { Autoplay } from "swiper";
-    import { SkeletonBlock } from '@skeleton-elements/svelte';
+    import { SkeletonBlock } from "@skeleton-elements/svelte";
+    import { Col, Row } from "sveltestrap";
 
     let recomendations = [];
+    let watchingNow = [];
+    let weekCollections = [];
 
-    onMount(async () => {
-        recomendations = await getRecomendations();
+    onMount(() => {
+        getRecomendations();
+        getWatchingNow();
+        getWeekCollections();
     });
 
     async function getRecomendations() {
@@ -24,12 +29,46 @@
             }
         );
         const json = await response.json();
-        return json.content;
+        recomendations = json.content;
     }
+
+    async function getWatchingNow() {
+        const response = await fetch(
+            `${CORS_BYPASS_SERVER}/https://api.anixart.tv/discover/watching/0`,
+            {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    ua: USER_AGENT,
+                },
+            }
+        );
+        const json = await response.json();
+        watchingNow = json.content;
+    }
+
+    async function getWeekCollections() {
+        const response = await fetch(
+            `${CORS_BYPASS_SERVER}/https://api.anixart.tv/collection/all/-1?previous_page=0&where=2&sort=4`,
+            {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    ua: USER_AGENT,
+                },
+            }
+        );
+        const json = await response.json();
+        weekCollections = json.content;
+    }
+
 </script>
 
+<!-- Swiper -->
 {#if recomendations.length === 0}
-    <SkeletonBlock effect="blink" height="400px" style="border-radius: 10px;"/>
+    <SkeletonBlock effect="blink" height="400px" style="border-radius: 10px;" />
 {:else}
     <Swiper
         slidesPerView={"auto"}
@@ -37,14 +76,14 @@
         spaceBetween={5}
         loop={true}
         autoplay={{
-            delay: 3000,
+            delay: 5000,
             disableOnInteraction: false,
         }}
         modules={[Autoplay]}
         class="mySwiper"
     >
         {#each recomendations as title (title["@id"])}
-            <SwiperSlide>
+            <SwiperSlide style="max-width: 700px;">
                 <div class="image-div">
                     <img
                         src={title.image}
@@ -56,11 +95,84 @@
                         <p>{title.description}</p>
                     </div>
                 </div>
-                <!-- {title.title} -->
             </SwiperSlide>
         {/each}
     </Swiper>
 {/if}
+
+<div style="font-size: 25px;">
+    <Row>
+        <Col>Популярное</Col>
+        <Col>Расписание</Col>
+    </Row>
+    <Row>
+        <Col>Коллекции</Col>
+        <Col>Фильтр</Col>
+    </Row>
+    <Row>
+        <Col>Онгоинги</Col>
+        <Col>Рандом</Col>
+    </Row>
+</div>
+
+<h1>Смотрят сейчас:</h1>
+
+{#if watchingNow.length === 0}
+    <SkeletonBlock effect="blink" height="260px" style="border-radius: 10px;" />
+{:else}
+    <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={5}
+        freeMode={true}
+        class="mySwiper"
+    >
+        {#each watchingNow as title (title["@id"])}
+            <SwiperSlide style="max-width: 200px;">
+                <div class="image-div">
+                    <img
+                        src={title.image}
+                        alt="Постер {title.title_ru}"
+                        class="image"
+                        style="height: 260px;"
+                    />
+                    <div>
+                        <h1 style="font-size: 18px;">{title.title_ru}</h1>
+                    </div>
+                </div>
+            </SwiperSlide>
+        {/each}
+    </Swiper>
+{/if}
+
+<h1>Коллекции недели:</h1>
+{#if weekCollections.length === 0}
+    <SkeletonBlock effect="blink" height="200px" style="border-radius: 10px;" />
+{:else}
+    <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={5}
+        freeMode={true}
+        class="mySwiper"
+    >
+        {#each weekCollections as title (title["@id"])}
+            <SwiperSlide style="max-width: 600px;">
+                <div class="image-div">
+                    <img
+                        src={title.image}
+                        alt="Постер {title.title}"
+                        class="image"
+                        style="height: 200px;"
+                    />
+                    <div>
+                        <h1 style="font-size: 24px;">{title.title}</h1>
+                        <p>{title.description}</p>
+                    </div>
+                </div>
+            </SwiperSlide>
+        {/each}
+    </Swiper>
+{/if}
+
 
 <style>
     .image-div {
