c24536da0cb94b766ab1424b34e73b691e82f833
r3t4k3r <av.19@list.ru>
2022-09-30 00:13:05 +0300

    добавление скелетон либы

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import "swiper/css";
 	import "swiper/css/pagination";
+	import '@skeleton-elements/svelte/skeleton-elements.css';
 
 	import Navbar from "./components/Navbar.svelte";
 	import { Router } from "svelte-router-spa";
