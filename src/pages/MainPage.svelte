178b10fdfab382f64717848abb481db17bfb03f1
r3t4k3r <av.19@list.ru>
2022-10-24 17:26:45 +0300

    отображение описания, вся хуйня

--- src/pages/MainPage.svelte
+++ src/pages/MainPage.svelte
@@ -5,6 +5,7 @@
     import { Autoplay } from "swiper";
     import { SkeletonBlock } from "@skeleton-elements/svelte";
     import { Col, Row } from "sveltestrap";
+    import { Container } from "sveltestrap";
 
     let recomendations = [];
     let watchingNow = [];
@@ -70,10 +71,7 @@
     >
         {#each new Array(3) as _}
             <SwiperSlide style="max-width: 700px;">
-                <SkeletonBlock
-                    height="400px"
-                    style="border-radius: 10px;"
-                />
+                <SkeletonBlock height="400px" style="border-radius: 10px;" />
             </SwiperSlide>
         {/each}
     </Swiper>
@@ -108,104 +106,109 @@
     </Swiper>
 {/if}
 
-<div style="font-size: 25px;">
-    <Row>
-        <Col>Популярное</Col>
-        <Col>Расписание</Col>
-    </Row>
-    <Row>
-        <Col>Коллекции</Col>
-        <Col>Фильтр</Col>
-    </Row>
-    <Row>
-        <Col>Онгоинги</Col>
-        <Col>Рандом</Col>
-    </Row>
-</div>
+<Container>
+    <div style="font-size: 25px;">
+        <Row>
+            <Col>Популярное</Col>
+            <Col>Расписание</Col>
+        </Row>
+        <Row>
+            <Col>Коллекции</Col>
+            <Col>Фильтр</Col>
+        </Row>
+        <Row>
+            <Col>Онгоинги</Col>
+            <Col>Рандом</Col>
+        </Row>
+    </div>
 
-<h1>Смотрят сейчас:</h1>
-{#if watchingNow.length === 0}
-    <Swiper
-        slidesPerView={"auto"}
-        spaceBetween={5}
-        freeMode={true}
-        class="mySwiper"
-    >
-        {#each new Array(10) as _}
-            <SwiperSlide style="max-width: 200px;">
-                <SkeletonBlock
-                    height="260px"
-                    style="border-radius: 10px;"
-                />
-            </SwiperSlide>
-        {/each}
-    </Swiper>
-{:else}
-    <Swiper
-        slidesPerView={"auto"}
-        spaceBetween={5}
-        freeMode={true}
-        class="mySwiper"
-    >
-        {#each watchingNow as title (title["@id"])}
-            <SwiperSlide style="max-width: 200px;">
-                <div class="image-div">
-                    <img
-                        src={title.image}
-                        alt="Постер {title.title_ru}"
-                        class="image"
-                        style="height: 260px;"
+    <h1>Смотрят сейчас:</h1>
+    {#if watchingNow.length === 0}
+        <Swiper
+            slidesPerView={"auto"}
+            spaceBetween={5}
+            freeMode={true}
+            class="mySwiper"
+        >
+            {#each new Array(10) as _}
+                <SwiperSlide style="max-width: 200px;">
+                    <SkeletonBlock
+                        height="260px"
+                        style="border-radius: 10px;"
                     />
-                    <div>
-                        <h1 style="font-size: 18px;">{title.title_ru}</h1>
+                </SwiperSlide>
+            {/each}
+        </Swiper>
+    {:else}
+        <Swiper
+            slidesPerView={"auto"}
+            spaceBetween={5}
+            freeMode={true}
+            class="mySwiper"
+        >
+            {#each watchingNow as title (title["@id"])}
+                <SwiperSlide style="max-width: 200px;">
+                    <div class="image-div">
+                        <img
+                            src={title.image}
+                            alt="Постер {title.title_ru}"
+                            class="image"
+                            style="height: 260px;"
+                        />
+                        <div style="max-width: 160px">
+                            <h1 style="font-size: 18px; word-break: break-word;bottom: 10px">{title.title_ru}</h1>
+                        </div>
                     </div>
-                </div>
-            </SwiperSlide>
-        {/each}
-    </Swiper>
-{/if}
+                </SwiperSlide>
+            {/each}
+        </Swiper>
+    {/if}
 
-<h1>Коллекции недели:</h1>
-{#if weekCollections.length === 0}
-    <Swiper
-        slidesPerView={"auto"}
-        spaceBetween={5}
-        freeMode={true}
-        class="mySwiper"
-    >
-        {#each new Array(3) as _}
-            <SwiperSlide style="max-width: 600px;">
-                <SkeletonBlock
-                    style="border-radius: 10px;height: 200px"
-                />
-            </SwiperSlide>
-        {/each}
-    </Swiper>
-{:else}
-    <Swiper
-        slidesPerView={"auto"}
-        spaceBetween={5}
-        freeMode={true}
-        class="mySwiper"
-    >
-        {#each weekCollections as title (title["@id"])}
-            <SwiperSlide style="max-width: 600px;">
-                <div class="image-div">
-                    <img
-                        src={title.image}
-                        alt="Постер {title.title}"
-                        class="image"
-                        style="height: 200px;"
-                    />
-                    <div>
-                        <h1 style="font-size: 24px;">{title.title}</h1>
-                        <p>{title.description}</p>
+    <h1>Коллекции недели:</h1>
+    {#if weekCollections.length === 0}
+        <Swiper
+            slidesPerView={"auto"}
+            spaceBetween={5}
+            freeMode={true}
+            class="mySwiper"
+        >
+            {#each new Array(3) as _}
+                <SwiperSlide style="max-width: 600px;">
+                    <SkeletonBlock style="border-radius: 10px;height: 200px" />
+                </SwiperSlide>
+            {/each}
+        </Swiper>
+    {:else}
+        <Swiper
+            slidesPerView={"auto"}
+            spaceBetween={5}
+            freeMode={true}
+            class="mySwiper"
+        >
+            {#each weekCollections as title (title["@id"])}
+                <SwiperSlide style="max-width: 600px;">
+                    <div class="image-div">
+                        <img
+                            src={title.image}
+                            alt="Постер {title.title}"
+                            class="image"
+                            style="height: 300px;"
+                        />
+                        <div style="max-width: 560px">
+                            <h1 style="font-size: 24px;">{title.title}</h1>
+                            {#if title.description.length !== 0}
+                                <p style="word-break: break-word;">
+                                    {title.description.substring(0, 64)}
+                                    {#if title.description.length > 64}...{/if}
+                                </p>
+                            {/if}
+                        </div>
                     </div>
-                </div>
-            </SwiperSlide>
-        {/each}
-    </Swiper>
-{/if}
+                </SwiperSlide>
+            {/each}
+        </Swiper>
+    {/if}
+</Container>
 
 <style>
     .image-div {
@@ -225,8 +228,8 @@
         text-align: left;
         color: white;
         position: absolute;
-        bottom: 20px;
-        left: 20px;
+        bottom: 0px;
+        left: 0px;
         width: 100%;
     }
 </style>
