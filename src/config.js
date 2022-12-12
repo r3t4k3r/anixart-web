9a1c708e9e307f7ae0c3de67fe6551e7b5f55b45
r3t4k3r <av.19@list.ru>
2022-12-13 02:20:30 +0300

    новые поля в конфиге

--- src/config.js
+++ src/config.js
@@ -1,4 +1,12 @@
-const CORS_BYPASS_SERVER = `http://46.175.148.75:5000/`
+const CORS_BYPASS_SERVER = `http://46.175.148.75:5000`
+const ANIXART_API_URL = `https://api.anixart.tv`
+const FULL_URL = `${CORS_BYPASS_SERVER}/${ANIXART_API_URL}`
 const USER_AGENT = 'AnixartApp'
 
-export {CORS_BYPASS_SERVER, USER_AGENT};
+const RCODE = {
+    LOADING: -1,
+    OK: 0,
+    SOMETHING: 1,
+}
+
+export { CORS_BYPASS_SERVER, ANIXART_API_URL, USER_AGENT, FULL_URL, RCODE };
