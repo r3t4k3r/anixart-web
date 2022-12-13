0b19125517e9122dfc06531e5ebf544a6d2e69f1
r3t4k3r <av.19@list.ru>
2022-12-14 02:25:42 +0300

    add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,34 @@
 import MainPage from './pages/MainPage.svelte';
 import NotFoundPage from './pages/NotFoundPage.svelte';
-
+import ReleasePage from './pages/ReleasePage/ReleasePage.svelte'
+import ReleasePageRecomends from './pages/ReleasePage/ReleasePageRecomends.svelte'
+import ReleasePageRelated from './pages/ReleasePage/ReleasePageRelated.svelte'
+import CollectionPage from './pages/CollectionPage.svelte'
 
 const routes = [
     {
         name: `/`,
-        path: '/',
         component: MainPage
+    },
+
+    // ReleasePage
+    {
+        name: '/release/:releaseId',
+        component: ReleasePage
+    },
+    {
+        name: '/release_related/:releaseId',
+        component: ReleasePageRelated
+    },
+    {
+        name: '/release_recomends/:releaseId',
+        component: ReleasePageRecomends
+    },
+
+    // CollectionPage
+    {
+        name: '/collection/:collectionId',
+        component: CollectionPage
     },
     {
         name: '404',
