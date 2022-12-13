78e082e33751cf3db99ed049ce0c855cba6777dc
r3t4k3r <av.19@list.ru>
2022-12-14 02:25:17 +0300

    add change route im MainPage

--- src/pages/MainPage.svelte
+++ src/pages/MainPage.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { api } from "../api/api";
-
+    import { navigateTo } from 'svelte-router-spa'
     import { RCODE } from "../config";
     import { Container, Button, Col, Row } from "sveltestrap";
     import Interesting from "../components/MainPage/Interesting.svelte";
@@ -18,43 +18,59 @@
         api.collection.all().then((r) => (weekCollections = r));
     });
 
-    const clickInterestingTitleCallback = (event) =>
-        console.log("event:", event);
+    const clickTitleCallback = (dpEvent) => {
+        const releaseId = dpEvent.detail.id
+        console.log(`/release/${releaseId}/`)
+        navigateTo(`/release/${releaseId}/`)
+    }
+    const clickCollectionCallback = (dpEvent) => {
+        const collectionId = dpEvent.detail.id
+        navigateTo(`/collection/${collectionId}/`)
+    }
+
 </script>
 
 <Interesting
     code={interesting.code}
     content={interesting.content}
-    on:clickBlock={clickInterestingTitleCallback}
+    on:clickBlock={clickTitleCallback}
 />
 
 <Container>
     <Row>
         <Col class="col-xxl-4 col-xl-4 col-lg-4 col-sm-6 col-6">
-            <Button class="mt-3" block outline color="dark">Популярное</Button>
+            <Button class="mt-3" block outline color="secondary">Популярное</Button>
         </Col>
         <Col class="col-xxl-4 col-xl-4 col-lg-4 col-sm-6 col-6">
-            <Button class="mt-3" block outline color="dark">Коллекции</Button>
+            <Button class="mt-3" block outline color="secondary">Коллекции</Button>
         </Col>
         <Col class="col-xxl-4 col-xl-4 col-lg-4 col-sm-6 col-6">
-            <Button class="mt-3" block outline color="dark">Расписание</Button>
+            <Button class="mt-3" block outline color="secondary">Расписание</Button>
         </Col>
         <Col class="col-xxl-4 col-xl-4 col-lg-4 col-sm-6 col-6">
-            <Button class="mt-3" block outline color="dark">Онгоинги</Button>
+            <Button class="mt-3" block outline color="secondary">Онгоинги</Button>
         </Col>
         <Col class="col-xxl-4 col-xl-4 col-lg-4 col-sm-6 col-6">
-            <Button class="mt-3" block outline color="dark">Фильтр</Button>
+            <Button class="mt-3" block outline color="secondary">Фильтр</Button>
         </Col>
         <Col class="col-xxl-4 col-xl-4 col-lg-4 col-sm-6 col-6">
-            <Button class="mt-3" block outline color="dark">Рандом</Button>
+            <Button class="mt-3" block outline color="secondary">Рандом</Button>
         </Col>
     </Row>
 
     <h1>Смотрят сейчас</h1>
-    <WatchNow code={watchingNow.code} content={watchingNow.content} />
+    <WatchNow
+        code={watchingNow.code}
+        content={watchingNow.content}
+        on:clickBlock={clickTitleCallback}
+    />
 
     <h1>Коллекции недели</h1>
-    <Collection code={weekCollections.code} content={weekCollections.content} />
+    <Collection
+        code={weekCollections.code}
+        content={weekCollections.content}
+        on:clickBlock={clickCollectionCallback}
+    />
 </Container>
 
 <style>
